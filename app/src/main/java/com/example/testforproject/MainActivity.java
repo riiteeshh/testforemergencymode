c4ae580323747f0ba21aea577a9736930b825f2f
riiteeshh <rp140578@gmail.com>
2021-12-17 21:54:58 +0545

    testing the emergency mode

--- app/src/main/java/com/example/testforproject/MainActivity.java
+++ app/src/main/java/com/example/testforproject/MainActivity.java
@@ -1,17 +1,27 @@
 package com.example.testforproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.KeyEvent;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.math.BigInteger;
 import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
     TextView check;
-    int n=0; // to check no of button press
+    int n=0;// to check no of button press
+    String p ="9865762048";
     @Override
 
     public boolean onKeyDown(int keyCode, KeyEvent event) {
@@ -40,8 +50,18 @@ public class MainActivity extends AppCompatActivity {
         }
 
         if(n==5){
+
             check.setText("Emergency mode started"); // process after count for button is 5
+            // checking the permission for the calling system
+            if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE)== PackageManager.PERMISSION_GRANTED){
+                callPhone();} // calling function
+                else
+                ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.CALL_PHONE},101);
+
+
             n=0;
+
+
 
         }}
 
@@ -49,17 +69,26 @@ public class MainActivity extends AppCompatActivity {
         return super.onKeyDown(keyCode, event);
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==101 && grantResults.length > 0 && grantResults[0]== PackageManager.PERMISSION_GRANTED){ // checking if the permission is granted
+            callPhone();
+        }
+        else
+            Toast.makeText(this, "Please accept all the permissions", Toast.LENGTH_SHORT).show();
+    }
+
+    private void callPhone() { // function to call
+        Intent intent= new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse("tel: "+ p));
+        startActivity(intent);
+    }
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
-
-
-
     }
 }
